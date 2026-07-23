cd62e956e51cbca4ea4d6b645687f8d5dcb565f3
Farhang Younis <comp-sci@hotmail.com>
2026-07-23 13:29:26 +0200

    Add Setup ef core basic setup

--- src/WebApi/Data/AppDbContext.cs
+++ src/WebApi/Data/AppDbContext.cs
@@ -1,6 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Features.Project;
+using WebApi.Features.User;
+
 namespace WebApi.Data;
 
-public class AppDbContext
+public class AppDbContext:DbContext
 {
-    
+    public DbSet<User> Users { get; set; }
+    public DbSet<Project>  Projects { get; set; }
+    public DbSet<Task>  Tasks { get; set; }
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        :base(options)
+    {
+        
+    }
 }
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
